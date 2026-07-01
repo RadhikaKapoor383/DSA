caef95dd78dfad72d2d4a4494bb968313ecdcbba
Radhika kapoor <radhikakapoor383@gmail.com>
2026-07-02 03:37:39 +0500

    Time: 0 ms (100%), Space: 48.2 MB (55%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int top=0;
+        int end = s.length-1;
+        
+        while(top<=end){
+            char temp = s[top];
+            s[top] = s[end];
+            s[end] = temp;
+            top++;
+            end--;
+        }
+        
+    }
+}
